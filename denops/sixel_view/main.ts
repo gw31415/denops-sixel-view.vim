d4101623e7171eeb18f2b97bca8492c4ff72538b
kamecha <masakikameyama81@gmail.com>
2024-08-18 20:29:58 +0900

    use assert for type check

--- denops/sixel_view/main.ts
+++ denops/sixel_view/main.ts
@@ -1,13 +1,14 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.1.0/mod.ts";
 import { img2sixel } from "./sixel.ts";
-import { isString } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts#^.ts";
+import {
+	assert,
+	isString,
+} from "https://deno.land/x/unknownutil@v3.10.0/mod.ts#^.ts";
 
 export function main(denops: Denops): Promise<void> {
 	denops.dispatcher = {
 		img2sixel(source, opts = {}) {
-			if (!isString(source)) {
-				throw new Error("not string value");
-			}
+			assert(source, isString);
 			return img2sixel(source, opts!);
 		},
 	};
