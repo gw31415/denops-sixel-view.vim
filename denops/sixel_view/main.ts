5a75742f088f92b6e23f225497950b533ea39097
kamecha <masakikameyama81@gmail.com>
2024-08-19 08:56:00 +0900

    refactor: use isInstanceOf for Uint8Array check

--- denops/sixel_view/main.ts
+++ denops/sixel_view/main.ts
@@ -2,16 +2,15 @@ import { Denops } from "https://deno.land/x/denops_std@v5.1.0/mod.ts";
 import { img2sixel } from "./sixel.ts";
 import {
 	assert,
+	isInstanceOf,
 	isOneOf,
 	isString,
 } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts#^.ts";
 
-const isUint8Array = (x: unknown): x is Uint8Array => x instanceof Uint8Array;
-
 export function main(denops: Denops): Promise<void> {
 	denops.dispatcher = {
 		img2sixel(source, opts = {}) {
-			assert(source, isOneOf([isString, isUint8Array]));
+			assert(source, isOneOf([isString, isInstanceOf(Uint8Array)]));
 			return img2sixel(source, opts!);
 		},
 	};
